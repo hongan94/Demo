8f58e5f624b5715e557d8e978bd822173ebc58b8
hongan94 <phamhongan2201@gmail.com>
2016-05-25 11:15:45 +0700

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+
+gem 'bootstrap-sass',       '3.2.0.0'
 gem 'rails',        '4.2.2'
 gem 'bcrypt',               '3.1.7'
 gem 'sass-rails',   '5.0.2'
@@ -20,4 +22,5 @@ end
 group :production do
   gem 'pg',             '0.17.1'
   gem 'rails_12factor', '0.0.2'
+  gem 'puma',           '3.1.0'
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,43 @@
 class UsersController < ApplicationController
-  def new
+  def home
   end
+
+  def index
+  	@user =User.new
+  end
+
+  def show
+  	 @user = User.find(params[:id])
+
+  end
+
+  def new
+  	    @user = User.new
+
+  end
+
+   def create
+   	    @user = User.new(user_params)
+    if @user.save
+       flash[:success] = "Welcome to the Sample App!"
+           redirect_to @user
+    else
+      render 'new'
+    end
+   end
+   private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+  # def edit
+  # end
+
+  # def update
+  # end
+
+  # def destroy
+  # end
 end
