1eff2215020e91dfe313a9201e07ae3847a4b1ad
hongan94 <phamhongan2201@gmail.com>
2016-05-26 10:11:38 +0700

    Finish log in/ log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,4 @@
 class UsersController < ApplicationController
-  def home
-  end
-
-  def index
-  	@user =User.new
-  end
-
   def show
   	 @user = User.find(params[:id])
 
@@ -19,11 +12,12 @@ class UsersController < ApplicationController
    def create
    	    @user = User.new(user_params)
     if @user.save
+            log_in @user
        flash[:success] = "Welcome to the Sample App!"
-           redirect_to @user
+           redirect_to @user 
     else
       render 'new'
-    end
+          end
    end
    private
 
